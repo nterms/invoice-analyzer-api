ef4fad0e7f19a1c022e562b3c430679f7ba65046
Saranga Abeykoon <saranga@wonderlabs.io>
2024-12-16 07:05:13 +0530

    Scaffolding invoice and invoice item resources

---
+++ database/migrations/2024_12_16_010120_create_invoice_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+            $table->string('item_name')->nullable();
+            $table->integer('quantity')->nullable()->default(0);
+            $table->decimal('price_per_unit', 10, 2)->nullable();
+            $table->decimal('total_price', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_items');
+    }
+};
